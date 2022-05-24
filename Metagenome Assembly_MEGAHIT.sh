98410757e1ed741b75f8c67cfdd7f8b2c906988e
Charmaine Khaye Alba <82104435+cdalba@users.noreply.github.com>
2022-05-24 11:33:23 +0800

    Update Metagenome Assembly_MEGAHIT.sh

--- Metagenome Assembly_MEGAHIT.sh
+++ Metagenome Assembly_MEGAHIT.sh
@@ -1,25 +1,20 @@
 #!/bin/bash
-#SBATCH --partition=batch
-#SBATCH --qos=240c-1h_batch
-#SBATCH --nodes=1
-#SBATCH --ntasks-per-node=64
-#SBATCH --mem=64G
-#SBATCH --job-name="Myxo_G1_MEGAHIT"
-#SBATCH --output=Myxo_G1_MEGAHIT.%J.out
 
-#SBATCH --error=Myxo_G1_MEGAHIT.%J.err
-#SBATCH --mail-user=cdalba@up.edu.ph
-#SBATCH --mail-type=ALL
+###############################################################################################################################################
+#
+#                     This Bash script is written by C.D. Alba.
+#
+#             This script performs the metagenome assembly step for the assembly-based method. 
+#             
+#         You can use any terminal to run this script. Ensure that all required software is installed and that all required files are available. 
+#
+###############################################################################################################################################
 
-echo "SLURM_JOBID="$SLURM_JOBID
-echo "SLURM_JOB_NODELIST"=$SLURM_JOB_NODELIST
-echo "SLURM_NNODES"=$SLURM_NNODES
-echo "SLURMTMPDIR="$SLURMTMPDIR
-echo "working directory="$SLURM_SUBMIT_DIR
+#insert SLURM/sbatch variables here if needed
 
 ulimit -s unlimited 
 
-cd /scratch1/charmaine.alba/G1
+cd /path/to/raw/data &&\ 
 
 module load anaconda/3-2021.11
 source activate myxocnda
