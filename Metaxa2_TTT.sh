7a0b8c32abb63a66b1e2ff0fb2a90597d0f321b5
Charmaine Khaye Alba <82104435+cdalba@users.noreply.github.com>
2022-05-24 11:37:11 +0800

    Update Metaxa2_TTT.sh

--- Metaxa2_TTT.sh
+++ Metaxa2_TTT.sh
@@ -1,25 +1,20 @@
 #!/bin/bash
-#SBATCH --partition=debug
-#SBATCH --qos=240c-1h_debug
-#SBATCH --nodes=1
-#SBATCH --ntasks-per-node=64
-#SBATCH --mem=64G
-#SBATCH --job-name="G1_Metaxa_TTT"
-#SBATCH --output=G1_Metaxa_TTT.%J.out
 
-#SBATCH --error=G1_Metaxa_TTT.%J.err
-#SBATCH --mail-user=cdalba@up.edu.ph
-#SBATCH --mail-type=ALL
+###############################################################################################################################################
+#
+#                     This Bash script is written by C.D. Alba.
+#
+#             This script can be used for summarizing the output of the taxonomic profiling.
+#             
+#         You can use any terminal to run this script. Ensure that all required software is installed and that all required files are available. 
+#
+###############################################################################################################################################
 
-echo "SLURM_JOBID="$SLURM_JOBID
-echo "SLURM_JOB_NODELIST"=$SLURM_JOB_NODELIST
-echo "SLURM_NNODES"=$SLURM_NNODES
-echo "SLURMTMPDIR="$SLURMTMPDIR
-echo "working directory="$SLURM_SUBMIT_DIR
+#insert SLURM/sbatch variables here if needed
 
 ulimit -s unlimited 
 
-cd /scratch3/charmaine.alba
+cd /path/to/raw/data &&\ 
 
 module load anaconda/3-2021.11
 source activate metaxa
